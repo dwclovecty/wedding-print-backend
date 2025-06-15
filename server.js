bc282672783577a33bfdff5b2da589c0dd80391e
dwclovecty <a14913933@gmail.com>
2025-06-16 00:04:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit '30mb' }));
+app.use(express.json({ limit: '30mb' }));
 
 const transporter = nodemailer.createTransport({
   host 'smtp.gmail.com',
